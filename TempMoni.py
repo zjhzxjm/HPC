398f9b0d6d35386b5659e2e74bf2c528196c4df6
zjhzxjm <416279164@qq.com>
2016-06-22 15:02:17 +0800

    Initial commit

---
+++ TempMoni.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+"""
+Author: xujm@realbio.cn
+Ver:2.0
+Add tts voice alert when the temperature above the critical level
+Ver:1.0
+init
+"""
+
+import os
+import re
+import subprocess
+import json
+import argparse
+import logging
+import top.api
+import settings
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument('-s', '--smsphone', dest='smsphone', type=str, help='Send sms alert to this phone', required=True)
+parser.add_argument('-t', '--ttsphone', dest='ttsphone', type=str, help='Send voice alert to this phone', required=True)
+parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help='Enable debug info')
+parser.add_argument('--version', action='version', version='2.0')
+
+
+class NotifyTool:
+    def __init__(self, sms_phone, tts_phone, host_name, dict_temp):
+        self.sms_phone = sms_phone.strip()
+        self.tts_phone = tts_phone.strip()
+        self.host_name = host_name
+        self.dict_temp = dict_temp
+        self.appkey = settings.ALIDAYU_APPKEY
+        self.secret = settings.ALIDAYU_SECRET
+
+    def sms_temp_warn(self):
+        logging.debug('sms config: {0} {1}, in_temp {2}'.format(self.appkey, self.secret, self.dict_temp['in_temp']))
+        req = top.api.AlibabaAliqinFcSmsNumSendRequest()
+        req.set_app_info(top.appinfo(self.appkey, self.secret))
+        # req.extend = "123456"
+        req.sms_type = "normal"
+        req.sms_free_sign_name = "锐翌集群"
+        req.sms_param = json.dumps({'hostname': self.host_name, 'in_temp': str(self.dict_temp['in_temp']),
+                                    'ex_temp': str(self.dict_temp['ex_temp'])})
+        req.rec_num = self.sms_phone
+        req.sms_template_code = "SMS_10845500"
+        try:
+            resp = req.getResponse()
+            return 1
+        except Exception as e:
+            logging.info('sms error {0}'.format(e))
+            return 0
+
+    def voice_temp_crit(self):
+        logging.debug('voice config: {0} {1}, in_temp {2}'.format(self.appkey, self.secret, self.dict_temp['in_temp']))
+        req = top.api.AlibabaAliqinFcTtsNumSinglecallRequest()
+        req.set_app_info(top.appinfo(self.appkey, self.secret))
+
+        # req.extend="12345"
+        req.tts_param = json.dumps({'hostname': self.host_name, 'in_temp': str(self.dict_temp['in_temp']),
+                                    'ex_temp': str(self.dict_temp['ex_temp'])})
+        req.called_num = self.tts_phone
+        req.called_show_num = "051482043271"
+        req.tts_code = "TTS_10970049"
+        try:
+            resp = req.getResponse()
+            return 1
+        except Exception as e:
+            logging.info('voice error {0}'.format(e))
+            return 0
+
+class IpmiTool:
+    def __init__(self, host_name):
+        self.host_name = host_name
+
+    def get_temp(self):
+        handle = os.popen('/opt/rocks/bin/rocks run host {0} "ipmitool -c sdr type temperature"'.format(self.host_name))
+        for i in handle:
+            if re.search('^FP', i) or re.search('^Inlet', i):
+                logging.debug('inlet line {0}'.format(i))
+                in_temp = int(i.split(',')[1])
+            elif re.search('^MB', i) or re.search('^Exhaust', i):
+                logging.debug('exhaust line {0}'.format(i))
+                ex_temp = int(i.split(',')[1])
+        return {'in_temp': in_temp, 'ex_temp': ex_temp}
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    smsphone = args.smsphone
+    ttsphone = args.ttsphone
+    hosts = ['nas-0-1', 'nas-0-2', 'nas-0-3', 'nas-0-4', 'nas-0-5', 'nas-0-6', 'nas-0-7', 'nas-0-8', 'nas-0-t',
+             'data-0-1', 'data-0-2', 'data-0-3', 'data-0-4', 'data-0-6', 'data-0-7',
+             'nas-1-1']
+    ex_temp_warn = 50
+    in_temp_warn = 30
+    ex_temp_crit = 70
+    in_temp_crit = 42
+    sms_done_file = '/data_center_01/home/xujm/logs/sms.done'
+
+    if args.verbose:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="[%(asctime)s]%(name)s:%(levelname)s:%(message)s",
+            filename='debug.log'
+        )
+    else:
+        logging.basicConfig(
+            level=logging.INFO,
+            format="[%(asctime)s]%(name)s:%(levelname)s:%(message)s",
+            filename='/data_center_01/home/xujm/logs/TempMoniInfo.log'
+        )
+
+    for host in hosts:
+        obj_ipmi = IpmiTool(host)
+        d_temp = obj_ipmi.get_temp()
+        obj_notify = NotifyTool(smsphone, ttsphone, host, d_temp)
+
+        # Send tts voice alert, when the temperature above critical level
+        if d_temp['ex_temp'] >= ex_temp_crit or d_temp['in_temp'] >= in_temp_crit:
+            recode = obj_notify.voice_temp_crit()
+            if recode:
+                logging.info('Voice send ok: {0}'.format(recode))
+            else:
+                logging.info('Voice not send: {0}'.format(recode))
+
+        # Send sms alert, when the temperature above warning level
+        if d_temp['ex_temp'] >= ex_temp_warn or d_temp['in_temp'] >= in_temp_warn:
+            if not os.path.exists(sms_done_file):
+                recode = obj_notify.sms_temp_warn()
+                if recode:
+                    logging.debug('Touch sms.done ok: {0}'.format(recode))
+                    subprocess.call(['touch', sms_done_file])
+                    logging.info('Temp:{0} - {1} - {2}'.format(host, d_temp['in_temp'], d_temp['ex_temp']))
+                    break
+                else:
+                    logging.info('SMS not send: {0}'.format(recode))
+        else:
+            recode = subprocess.call(['rm', sms_done_file])
+            logging.debug('Remove sms.done return code: {0}'.format(recode))
